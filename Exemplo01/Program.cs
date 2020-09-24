df9f2f87507d12f6826eff6db4612fb38b716a36
Matheus Augusto Machado <matheusaugustomaachado@gmail.com>
2020-09-24 15:07:12 -0300

    Escrita Ola Mundo

---
+++ Exemplo01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exemplo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
